08759f424604eaea400165add1d6161d732b3c0d
Asmei <kamsykula@gmail.com>
2017-11-23 00:16:07 +0100

    Extract two method from class constructor - for left and right side

---
+++ core/src/com/epiklp/game/enemies/Enemy.java
@@ -0,0 +1,65 @@
+package com.epiklp.game.enemies;
+
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * Created by epiklp on 19.11.17.
+ */
+
+public abstract class Enemy {
+    private float strengh;
+    private float life;
+    private float speed;
+    private String name;
+    private float chanceOfDrop;
+
+    private Body body;
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public float getStrengh() {
+        return strengh;
+    }
+
+    public void setStrengh(float strengh) {
+        this.strengh = strengh;
+    }
+
+    public float getLife() {
+        return life;
+    }
+
+    public void setLife(float life) {
+        this.life = life;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getChanceOfDrop() {
+        return chanceOfDrop;
+    }
+
+    public void setChanceOfDrop(float chanceOfDrop) {
+        this.chanceOfDrop = chanceOfDrop;
+    }
+}
