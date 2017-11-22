e0f6c54a3ae33022ddfed59bde46b55ed72c8b4a
Asmei <kamsykula@gmail.com>
2017-11-22 23:34:54 +0100

    Extract two method from class constructor - for left and right side

--- core/src/com/epiklp/game/Controller.java
+++ core/src/com/epiklp/game/Controller.java
@@ -14,99 +14,26 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
  */
 
 public class Controller implements Interface {
-    private static final int BUTTON_SIZE = 10;
+    private static final int BUTTON_SIZE = width/10;
     private Stage stage;
     private boolean upPressed, attackPressed, leftPressed, rightPressed;
-    private Table tabr;
 
     public Controller() {
         stage = new Stage(new ExtendViewport(width, height));
         Gdx.input.setInputProcessor(stage);
 
-        Table tabl = new Table();
-        tabl.bottom().left();
+        Table tableLeft = new Table();
+        Table tableRight = new Table();
 
+        initLeftSide(tableLeft);
+        initRightSide(tableRight);
 
-        Image leftButt = new Image(new Texture("button/Left.png"));
-        leftButt.setSize(width / BUTTON_SIZE, width / BUTTON_SIZE);
-        leftButt.addListener(new InputListener() {
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                leftPressed = true;
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                leftPressed = false;
-            }
-        });
-        Image rightButt = new Image(new Texture("button/Right.png"));
-        rightButt.setSize(width / BUTTON_SIZE, width / BUTTON_SIZE);
-        rightButt.addListener(new InputListener() {
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                rightPressed = true;
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                rightPressed = false;
-            }
-        });
-        tabl.add(rightButt).size(leftButt.getWidth(), leftButt.getHeight());
-        tabl.row().padBottom(10);
-        tabl.add(leftButt).size(leftButt.getWidth(), leftButt.getHeight());
-        stage.addActor(tabl);
-
-        tabr = new Table();
-        tabr.setPosition(width - (width / BUTTON_SIZE) / 2, (width / BUTTON_SIZE));
-        Image upButt = new Image(new Texture("button/Up.png"));
-        upButt.setSize(width / BUTTON_SIZE, width / BUTTON_SIZE);
-        upButt.addListener(new InputListener() {
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                upPressed = true;
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                upPressed = false;
-            }
-        });
-
-        Image attackButt = new Image(new Texture("button/Attack.png"));
-        attackButt.setSize(width / BUTTON_SIZE, width / BUTTON_SIZE);
-        attackButt.addListener(new InputListener() {
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                attackPressed = true;
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                attackPressed = false;
-            }
-        });
-
-        tabr.add(attackButt).size(leftButt.getWidth(), leftButt.getHeight());
-        tabr.row().padBottom(10);
-        tabr.add(upButt).size(leftButt.getWidth(), leftButt.getHeight());
-        stage.addActor(tabr);
-
-        setTableDebugMode(tabl);
-        setTableDebugMode(tabr);
+        setTableDebugMode(tableLeft);
+        setTableDebugMode(tableRight);
     }
 
-    private void setTableDebugMode(Table tabl) {
-        tabl.setDebug(true);
+    private void setTableDebugMode(Table table) {
+        table.setDebug(true);
     }
 
     public void draw() {
@@ -132,5 +59,85 @@ public class Controller implements Interface {
 
     public void dispose() {
         stage.dispose();
+    }
+
+    private void initLeftSide(Table tableLeft) {
+        tableLeft.bottom().left();
+        Image leftButt = new Image(new Texture("button/Left.png"));
+        leftButt.setSize(BUTTON_SIZE, BUTTON_SIZE);
+        leftButt.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                leftPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                leftPressed = false;
+            }
+        });
+        Image rightButt = new Image(new Texture("button/Right.png"));
+        rightButt.setSize(BUTTON_SIZE, BUTTON_SIZE);
+        rightButt.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                rightPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                rightPressed = false;
+            }
+        });
+        tableLeft.columnDefaults(1).width(BUTTON_SIZE);
+        tableLeft.add(leftButt).size(BUTTON_SIZE, BUTTON_SIZE);
+        tableLeft.add(rightButt).size(BUTTON_SIZE, BUTTON_SIZE);
+
+        stage.addActor(tableLeft);
+    }
+
+    private void initRightSide(Table tableRight) {
+        tableRight.setPosition(width - (BUTTON_SIZE) / 2, (BUTTON_SIZE));
+        Image upButt = new Image(new Texture("button/Up.png"));
+        upButt.setSize(BUTTON_SIZE, BUTTON_SIZE);
+        upButt.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                upPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                upPressed = false;
+            }
+        });
+
+        Image attackButt = new Image(new Texture("button/Attack.png"));
+        attackButt.setSize(BUTTON_SIZE, BUTTON_SIZE);
+        attackButt.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                attackPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                attackPressed = false;
+            }
+        });
+
+        tableRight.add(attackButt).size(BUTTON_SIZE, BUTTON_SIZE);
+        tableRight.row().padBottom(10);
+        tableRight.add(upButt).size(BUTTON_SIZE, BUTTON_SIZE);
+
+        stage.addActor(tableRight);
     }
 }
