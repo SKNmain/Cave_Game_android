42d1820383e80dd73ea62232a2725ed0d5f8a45a
Rafał Rybowski <rafalrybowski1996@gmail.com>
2018-02-06 19:09:32 +0100

    new

--- core/src/com/epiklp/game/Functional/TheBox.java
+++ core/src/com/epiklp/game/Functional/TheBox.java
@@ -126,7 +126,7 @@ public class TheBox {
         RayHandler.setGammaCorrection(false);
         RayHandler.useDiffuseLight(false);
         rayHandler = new RayHandler(TheBox.world);
-        rayHandler.setAmbientLight(1);
+        rayHandler.setAmbientLight(.2f);
         rayHandler.setCulling(true);
         rayHandler.setShadows(true);
         Light.setGlobalContactFilter(TheBox.CATEGORY_LIGHT, (short) 0, TheBox.MASK_LIGHT);
