32f31c95f0967bf86422ebfc24c8dfd9848babab
Asmei <kamsykula@gmail.com>
2018-02-07 20:20:38 +0100

    Add new enemies; fix jumping (again)
    Add spirder and rat, fix jump conditional

--- core/src/com/epiklp/game/Game/GameScreen.java
+++ core/src/com/epiklp/game/Game/GameScreen.java
@@ -179,7 +179,7 @@ public class GameScreen implements Screen {
             hero.setState(Hero.STATE.CLIMBING);
         }
         else if (controller.isUpPressed()) {
-            hero.setState(Hero.STATE.JUMPING);
+            hero.setCanJump(true);
         }
 
 
--- core/src/com/epiklp/game/actors/characters/Spider.java
+++ core/src/com/epiklp/game/actors/characters/Spider.java
@@ -1,6 +1,5 @@
 package com.epiklp.game.actors.characters;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.epiklp.game.Cave;
@@ -11,18 +10,18 @@ import com.epiklp.game.Functional.TheBox;
  * Created by Asmei on 2017-11-29.
  */
 
-public class FlameDemon extends Enemy {
-    public FlameDemon(float x, float y) {
-        super(new Sprite(Assets.manager.get(Assets.flameDemon)));
-        sprite.setSize(1.4f * Cave.PPM * Cave.SCALE, 1.8f * Cave.PPM * Cave.SCALE);
+public class Spider extends Enemy {
+    public Spider(float x, float y) {
+        super(new Sprite(Assets.manager.get(Assets.spider)));
+        sprite.setSize(1.2f * Cave.PPM * Cave.SCALE, 1.0f * Cave.PPM * Cave.SCALE);
         sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2 + 1.f);
         body = TheBox.createBody(x, y, false);
-        TheBox.createBoxShape(body, 30f, 50f, 25f, 0);
-        TheBox.createBoxSensor(body, 200f, 90f, new Vector2(0, 45f), WARD_SENSOR);
+        TheBox.createBoxShape(body, 35f, 20f, 25f, 0);
+        TheBox.createBoxSensor(body, 150f, 70f, new Vector2(0, 45f), WARD_SENSOR);
         body.setUserData(this);
         body.setGravityScale(30f);
 
-        light = TheBox.createPointLight(body, 64, new Color(1.000f, 0.498f, 0.314f, .75f), 10, 0, 0);
+        //light = TheBox.createPointLight(body, 64, new Color(1.000f, 0.498f, 0.314f, .75f), 10, 0, 0);
 
         initStats();
 
@@ -30,12 +29,12 @@ public class FlameDemon extends Enemy {
 
     @Override
     public void initStats() {
-        this.life = 50;
+        this.life = 30;
         this.attackSpeed = 2;
         this.speedWalk = 4;
         this.strengh = 10;
         this.attackRange = 5f;
-        this.watchRange = 3f;
+        this.watchRange = 2f;
 
     }
 }
